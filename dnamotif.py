40833e00f06d3ac9443afb6de33641faf993e7e2
sandra-ct <103966006+sandra-ct@users.noreply.github.com>
2025-05-28 20:20:36 -0400

    Add files via upload

---
+++ dnamotif.py
@@ -0,0 +1,19 @@
+# rosalind - finding a motif in DNA
+
+import regex as re
+
+with open (r"Downloads\rosalind_subs (4).txt", 'r') as file:
+    content = file.read()
+seq_raw, sub_seq = content.splitlines() # seq = s, sub_seq = t
+
+seq = re.sub(' ', '', seq_raw)
+
+sub_locs = re.finditer(sub_seq,seq)
+matches = [sub.group() for sub in sub_locs]
+
+search_loc = re.search(sub_seq,seq)
+start_end_locations = [search.span() for search in re.finditer(sub_seq,seq)]
+
+all_iterations = [sub.start(0) for sub in re.finditer(sub_seq,seq)]
+
+print('Full Seq:\n', seq, '\nSearching for: ', sub_seq, '\n\nMatched Strings:', matches, '\n\n', 'Span of Matches:', start_end_locations, '\n\n', 'Locations of All Matches:', all_iterations)
